ad0525c1d5faed5391ff619e161b26d2ba7f08c8
Sai Sampath Kumar Raigiri <rsaisampathkumar@gmail.com>
2020-02-24 14:49:35 -0500

    unecessary
    asad

--- 9 custom-directive/src/app/cc-logo.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CcLogoDirective } from './cc-logo.directive';
-
-describe('CcLogoDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CcLogoDirective();
-    expect(directive).toBeTruthy();
-  });
-});
