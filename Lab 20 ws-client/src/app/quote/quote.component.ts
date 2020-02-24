f1e208300b5e0bee27f599b94f5f4103e83a045f
Sai Sampath Kumar Raigiri <rsaisampathkumar@gmail.com>
2020-02-24 15:44:05 -0500

    Lab 20

---
+++ Lab 20 ws-client/src/app/quote/quote.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote, QuoteService } from '../quote.service';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  lastQuote: Quote;
+
+  constructor(private quoteSvc: QuoteService) { }
+
+  ngOnInit(): void {
+    this.quoteSvc.getQuotes().subscribe(quote => this.lastQuote = quote);
+  }
+
+}
